9f75c120eb980ba810d67824ebcb580e173a9c0b
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-05-09 17:16:16 -0300

    Parser mas masticado

--- src/parser/SubscriptionParser.java
+++ src/parser/SubscriptionParser.java
@@ -11,12 +11,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SubscriptionParser extends GeneralParser{
-    public JSONObject ParserJson(String filePath) throws IOException {
+    public JSONObject ParserJson(String filePath) throws IOException { // Devuelve un JSONObject con el contenido del archivo
         String StingJson = new String(Files.readAllBytes(Paths.get(filePath)));
         JSONObject json = new JSONObject(StingJson);
         return json;
     }
-    public JSONArray filterUrlsByType(JSONObject json, String urlType){
+    public JSONArray filterUrlsByType(JSONObject json, String urlType){ // Devuelve un JSONArray con las urls filtradas por tipo
         JSONArray filteredUrls = new JSONArray();
         for (int i = 0 ; i < json.length() ; i++) { //Me fijo en cada suscripcion
             JSONObject subs = json.getJSONObject("subscriptions");
@@ -26,10 +26,26 @@ public class SubscriptionParser extends GeneralParser{
                 String url = subs.getString("url");
                 for (int j = 0 ; j < Params.length() ; j++){ // Me fijo en cada parametro de la suscripcion y lo agrego a la url filtrada
                     String param = Params.getString(j);
-                    filteredUrls.put(url.replace("%s", param));
+                    filteredUrls.put(url.replace("%s", param)); // Reemplazo el %s por el parametro
                 }
             }
         }
         return  filteredUrls;
+    }
+
+    @Override
+    public JSONObject responseParser(JSONObject json) {
+        JSONArray rssUrls = filterUrlsByType(json, "rss");
+        JSONArray redditUrls = filterUrlsByType(json, "reddit");
+        JSONObject response = new JSONObject();
+        response.put("rss", rssUrls);
+        response.put("reddit", redditUrls);
+        System.out.println(response.toString());
+        return response;
+    }
+    @Override
+    public JSONObject FileParser(String filePath) throws IOException{
+        JSONObject json = ParserJson(filePath); // Obtenemos el JSON del path
+        return responseParser(json); // Devolvemos la respuesta
     }
 }
