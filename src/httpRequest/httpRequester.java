a783c66d5cf0eb024cf5b19e9f9ae83e47bd736e
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-05-09 20:49:49 -0300

    Avance en el request, al menos una idea

--- src/httpRequest/httpRequester.java
+++ src/httpRequest/httpRequester.java
@@ -6,11 +6,31 @@ package httpRequest;
  * https://www.baeldung.com/java-http-request
  * */
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class httpRequester {
 	
-	public String getFeedRss(String urlFeed){
-		String feedRssXml = null;
-		return feedRssXml;
+	public String getFeedRss(String urlFeed) throws IOException {
+		URL url = new URL(urlFeed);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		if(conn.getResponseCode()==200){
+			BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String feedRssXml;
+			StringBuilder content = new StringBuilder();
+			while ((feedRssXml = read.readLine()) != null) {
+				content.append(feedRssXml);
+			}
+			read.close();
+			return content.toString();
+		} else {
+			throw new IOException("Failed to retrieve RSS feed. Response code: " + conn.getResponseCode());
+		}
 	}
 
 	public String getFeedReedit(String urlFeed) {
