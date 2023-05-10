230c04d738427afba46a755c5721a1be0dfa686e
Lautaro Bachmann <laurigbach@gmail.com>
2023-05-10 12:25:57 -0300

    Corregir error de path y bug en el que no se imprimian los posts
    - Cambiar separador de path a "/"
    
    - Corregir forma de comparar igualdad

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -9,13 +9,13 @@ public class FeedReaderMain {
 	private static void printHelp(){
 		System.out.println("Please, call this program in correct way: FeedReader [-ne]");
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("************* FeedReader version 1.0 *************");
 		if (args.length == 0) {
-			
+
 			// Leer el archivo de suscription por defecto y parsearlo
-			SubscriptionParser subscriptionParser = new SubscriptionParser("config\\subscriptions.json");
+			SubscriptionParser subscriptionParser = new SubscriptionParser("config/subscriptions.json");
 			Subscription subscription = subscriptionParser.parse();
 
 			// Llamar al httpRequester para obtener el feed del servidor
@@ -24,28 +24,28 @@ public class FeedReaderMain {
 
 			// Llamar al Parser especifico para extraer los datos necesarios por la aplicacion, instanciar los feeds e imprimirlos
 			for(RoughFeed roughFeed : roughFeeds){
-				if(roughFeed.getUrlType() == "rss"){
+				if(roughFeed.getUrlType().equals("rss")){
 					RssParser rssParser = new RssParser(roughFeed);
 					Feed feed = rssParser.parse();
 					feed.prettyPrint();
-				} else if (roughFeed.getUrlType() == "reddit"){
+				} else if (roughFeed.getUrlType().equals("reddit")){
 					RedditParser redditParser = new RedditParser(roughFeed);
 					Feed feed = redditParser.parse();
 					feed.prettyPrint();
 				}
-				
+
 			}
 		} else if (args.length == 1){
-			
+
 			/*
 			Leer el archivo de suscription por defecto;
 			Llamar al httpRequester para obtenr el feed del servidor
-			Llamar al Parser especifico para extrar los datos necesarios por la aplicacion 
+			Llamar al Parser especifico para extrar los datos necesarios por la aplicacion
 			Llamar al constructor de Feed
 			Llamar a la heuristica para que compute las entidades nombradas de cada articulos del feed
 			LLamar al prettyPrint de la tabla de entidades nombradas del feed.
 			 */
-			
+
 		}else {
 			printHelp();
 		}
