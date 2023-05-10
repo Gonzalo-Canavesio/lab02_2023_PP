e2470f8b11bd26aa74a8e9fd78deac41f8c8569c
Lautaro Bachmann <laurigbach@gmail.com>
2023-05-10 15:49:05 -0300

    Reemplazar condicional con polimorfismo

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -10,6 +10,16 @@ public class FeedReaderMain {
 		System.out.println("Please, call this program in correct way: FeedReader [-ne]");
 	}
 
+    private static FeedParser createParser(RoughFeed roughFeed) {
+        FeedParser result = null;
+        if(roughFeed.getUrlType().equals("rss")){
+            result = new RssParser(roughFeed);
+        } else if (roughFeed.getUrlType().equals("reddit")){
+            result = new RedditParser(roughFeed);
+        }
+        return result;
+    }
+
 	public static void main(String[] args) {
 		System.out.println("************* FeedReader version 1.0 *************");
 		if (args.length == 0) {
@@ -24,17 +34,16 @@ public class FeedReaderMain {
 
 			// Llamar al Parser especifico para extraer los datos necesarios por la aplicacion, instanciar los feeds e imprimirlos
 			for(RoughFeed roughFeed : roughFeeds){
-				if(roughFeed.getUrlType().equals("rss")){
-					RssParser rssParser = new RssParser(roughFeed);
-					Feed feed = rssParser.parse();
-					feed.prettyPrint();
-				} else if (roughFeed.getUrlType().equals("reddit")){
-					RedditParser redditParser = new RedditParser(roughFeed);
-					Feed feed = redditParser.parse();
-					feed.prettyPrint();
-				}
-
+                FeedParser parser = createParser(roughFeed);
+                try {
+                    Feed feed = parser.parse();
+                    feed.prettyPrint();
+                } catch(Exception e){
+                    e.printStackTrace();
+                    System.out.print("Invalid feed type");
+                }
 			}
+
 		} else if (args.length == 1){
 
 			/*
