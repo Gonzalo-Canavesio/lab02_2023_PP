27e32c54eb6c32b00213977fed1e9a3296f0661d
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-05-10 19:34:44 -0300

    RedditParser por ahora

--- src/httpRequest/httpRequester.java
+++ src/httpRequest/httpRequester.java
@@ -1,13 +1,16 @@
 package httpRequest;
-import java.net.HttpURLConnection;
+
+import subscription.SingleSubscription;
+import subscription.Subscription;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URI;
 import java.io.IOException;
-import subscription.*;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 /* Esta clase se encarga de realizar efectivamente el pedido de feed al servidor de noticias
  * Leer sobre como hacer una http request en java
@@ -77,9 +80,39 @@ public class httpRequester {
 		}
 	}
 
-	public String getFeedReddit(String urlFeed) {
-		String feedRedditJson = null;
-		return feedRedditJson;
+	public String getFeedReddit(String urlFeed){
+		URL url;
+		HttpURLConnection conn;
+		// Hago el pedido de feed al servidor de noticias
+		try{
+			url = (new URI(urlFeed)).toURL();
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+		} catch (Exception e){
+			System.out.println("Error al hacer el pedido de feed al servidor de noticias");
+			e.printStackTrace();
+			return null;
+		}
+		// Chequeo que el pedido haya sido exitoso, y en ese caso devuelvo el contenido del feed
+		try{
+			if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+				BufferedReader readd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String inputLinee;
+				StringBuilder response = new StringBuilder();
+				while ((inputLinee = readd.readLine()) != null) {
+					response.append(inputLinee);
+				}
+				readd.close();
+				return response.toString();
+			}else {
+				throw new IOException("HTTP request failed with code " + conn.getResponseCode());
+			}
+		}catch (Exception e){
+			System.out.println("Error al obtener el contenido del feed");
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
--- src/parser/RedditParser.java
+++ src/parser/RedditParser.java
@@ -1,6 +1,12 @@
 package parser;
+
+import feed.Article;
+import feed.Feed;
 import httpRequest.RoughFeed;
-import feed.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Date;
 
 /*
  * Esta clase implementa el parser de feed de tipo reddit (json)
@@ -15,6 +21,26 @@ public class RedditParser extends GeneralParser implements FeedParser{
     }
 
     public Feed parse() {
-        return null;
+        Feed ResultFeed = null;
+        try {
+            String JsonFeed = this.roughFeed.getFeed();
+            JSONObject feedz = new JSONObject(JsonFeed);
+
+            JSONArray items = feedz.getJSONObject("data").getJSONArray("children");
+            ResultFeed = new Feed(feedz.getJSONObject("data").getJSONArray("children").getJSONObject(0).getJSONObject("data").getString("subreddit"));
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject item = items.getJSONObject(i).getJSONObject("data");
+                String title = item.getString("title");
+                String link = item.getString("url");
+                String description = item.getString("selftext");
+                Date pubDate = new Date(item.getLong("created_utc") * 1000);
+                Article articulo = new Article(title, description, pubDate, link);
+                ResultFeed.addArticle(articulo);
+            }
+        }catch (Exception e) {
+            System.out.println("Error al parsear el feed reddit");
+            e.printStackTrace();
+        }
+            return ResultFeed;
+        }
     }
-}
