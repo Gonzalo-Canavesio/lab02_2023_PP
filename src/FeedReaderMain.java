2a349610f58cc43d388a6a7fd48be131908abd95
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2023-05-11 17:13:15 -0300

    Primera implementación del sistema de detección de entidades nombradas

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -1,8 +1,12 @@
 import parser.*;
 import subscription.*;
 import httpRequest.*;
+import namedEntity.NamedEntity;
+import namedEntity.heuristic.*;
+import feed.*;
 import java.util.List;
-import feed.Feed;
+import java.util.Map;
+import java.util.HashMap;
 
 public class FeedReaderMain {
 
@@ -47,11 +51,50 @@ public class FeedReaderMain {
 
 		} else if (args.length == 1){
 
+			// Leer el archivo de suscription por defecto y parsearlo
+			SubscriptionParser subscriptionParser = new SubscriptionParser("config/subscriptions.json");
+			Subscription subscription = subscriptionParser.parse();
+
+			// Llamar al httpRequester para obtener el feed del servidor
+			httpRequester httpRequester = new httpRequester(subscription);
+			List<RoughFeed> roughFeeds = httpRequester.getFeeds();
+
+			Map<String,NamedEntity> namedEntities = new HashMap<String,NamedEntity>();
+			Heuristic heuristic = new QuickHeuristic();
+			// Llamar al Parser especifico para extraer los datos necesarios por la aplicacion, instanciar los feeds y analizarlos con la heuristica
+			for(RoughFeed roughFeed : roughFeeds){
+                FeedParser parser = createParser(roughFeed);
+				// Si el feed es de tipo reddit, no se analiza ya que genera demasiado ruido
+				if(roughFeed.getUrlType().equals("reddit")){
+					continue;
+				}
+                try {
+                    Feed feed = parser.parse();
+                    for(Article article : feed.getArticleList()){
+					String[] words = article.getText().split(" ");
+						for(String word : words){
+							if(heuristic.isEntity(word)){
+								if(namedEntities.containsKey(word)){
+									namedEntities.get(word).incFrequency();
+								} else {
+									NamedEntity namedEntity = new NamedEntity(word,heuristic.getCategory(word),1);
+									namedEntities.put(word,namedEntity);
+								}
+							}
+						}
+					}
+
+                } catch(Exception e){
+                    e.printStackTrace();
+                    System.out.print("Invalid feed type");
+                }
+			}
+			// Llamar al prettyPrint de la tabla de entidades nombradas del feed.
+			for(NamedEntity namedEntity : namedEntities.values()){
+				namedEntity.prettyPrint();
+			}
+
 			/*
-			Leer el archivo de suscription por defecto;
-			Llamar al httpRequester para obtenr el feed del servidor
-			Llamar al Parser especifico para extrar los datos necesarios por la aplicacion
-			Llamar al constructor de Feed
 			Llamar a la heuristica para que compute las entidades nombradas de cada articulos del feed
 			LLamar al prettyPrint de la tabla de entidades nombradas del feed.
 			 */
